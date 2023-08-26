a926531b842828637f081eda8a2550b3e21a3f37
niceneet <sakamotoyuuke835@gmail.com>
2023-08-26 22:27:40 +0900

    カレンダー5回目

--- 02.calendar/calender.rb
+++ 02.calendar/calender.rb
@@ -7,14 +7,13 @@ m = params["m"]
 
 year = y.nil? ? Date.today.year : y.to_i
 month = m.nil? ? Date.today.month : m.to_i
-target_date = Date.new(year, month)
 
-puts target_date.strftime("%m月 %Y").center(21)
+first_date =  Date.new(year, month, 1)
+last_date = Date.new(year, month, -1)
+
+puts first_date.strftime("%-m月 %Y").center(20)
 puts "日 月 火 水 木 金 土"
-first_date =  Date.new(target_date.year, target_date.month, 1)
-last_date = Date.new(target_date.year, target_date.month, -1)
-first_wday = first_date.wday
-print "   " * first_wday
+print "   " * first_date.wday
 
 (first_date..last_date).each do |date|
   print date.day.to_s.rjust(2) + " "
