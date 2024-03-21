52b27e00dbbd5b3bbd5f4a0df5e69725b3ceb678
Sakamoto Yusuke <121929760+sakamoto835@users.noreply.github.com>
2024-03-21 15:40:49 +0900

    メソッド名、file_name修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -9,16 +9,16 @@ max_length = 0
 
 files.each do |file|
   file_name = File.basename(file)
-  file_length = Pathname.new(file_name).basename.to_s.length
+  file_length = file_name.length
   max_length = file_length if file_length > max_length
 end
 
-files_row = 3
-files_puts = (files.size / files_row.to_f).ceil
+Files_column = 3
+files_row = (files.size / Files_column.to_f).ceil
 
-(0...files_puts).each do |i|
-  files_row.times do |j|
-    index = i + j * files_puts
+(0...files_row).each do |i|
+  Files_column.times do |j|
+    index = i + j * files_row
     print File.basename(files[index]).ljust(max_length + 3) if files[index]
   end
   puts
