444c76e7a5583c222853553be468756ffcf18ef9
Sakamoto Yusuke <121929760+sakamoto835@users.noreply.github.com>
2024-03-24 20:59:26 +0900

    rubocopでの修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -4,13 +4,13 @@
 NUMBER_OF_COLUMNS = 3
 COLUMN_MARGIN = 3
 
-files = Dir.glob("*").sort
+files = Dir.glob('*').sort
 
 number_of_row = (files.size / NUMBER_OF_COLUMNS.to_f).ceil
 files_array = files.each_slice(number_of_row).to_a
 
 max_length_array = files_array.map do |elements|
-  elements.map {|file|file.length}.max
+  elements.map { |file| file.length }.max
 end
 
 (0...number_of_row).each do |row|
