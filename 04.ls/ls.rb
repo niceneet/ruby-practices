2d21b8fff1697d316bbed23f2b1f79a9784bde8f
Sakamoto Yusuke <121929760+sakamoto835@users.noreply.github.com>
2024-03-26 22:11:45 +0900

    メソッド作成

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -4,19 +4,30 @@
 NUMBER_OF_COLUMNS = 3
 COLUMN_MARGIN = 3
 
-files = Dir.glob('*').sort
+def main
+  files = Dir.glob('*').sort
 
-number_of_row = (files.size / NUMBER_OF_COLUMNS.to_f).ceil
-files_array = files.each_slice(number_of_row).to_a
+  number_of_row = (files.size / NUMBER_OF_COLUMNS.to_f).ceil
+  files_array = files.each_slice(number_of_row).to_a
 
-max_length_array = files_array.map do |elements|
-  elements.map { |file| file.length }.max
+  max_length_array = make_max_length_array(files_array)
+  output_ls(files, number_of_row, max_length_array)
 end
 
-(0...number_of_row).each do |row|
-  NUMBER_OF_COLUMNS.times do |column|
-    index = row + column * number_of_row
-    print files[index].ljust(max_length_array[column] + COLUMN_MARGIN) if files[index]
+def make_max_length_array(files_array)
+  files_array.map do |elements|
+    elements.map(&:length).max
   end
-  puts
 end
+
+def output_ls(files, number_of_row, max_length_array)
+  (0...number_of_row).each do |row|
+    NUMBER_OF_COLUMNS.times do |column|
+      index = row + column * number_of_row
+      print files[index].ljust(max_length_array[column] + COLUMN_MARGIN) if files[index]
+    end
+    puts
+  end
+end
+
+main
