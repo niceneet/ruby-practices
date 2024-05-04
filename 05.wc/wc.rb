76d2148a0f6df6b11a7a90c6788b146efa1050e4
Sakamoto Yusuke <121929760+sakamoto835@users.noreply.github.com>
2024-05-04 23:17:25 +0900

    入力されたオプションのみの表示機能

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -8,23 +8,21 @@ require 'debug'
 SIZE_INDENT = 8
 
 def main #ファイル引数なし
-	params = ARGV.getopts('lwc')
-	# options = parse_options
-  # options = { c: true, l: true, w: true } if options.empty?
-	files = ARGV
+	p params = ARGV.getopts('lwc')
+	files = ARGV 
 	if files.empty?
 		lines = $stdin.read
 	end
-	total_array = [0, 0, 0]
+	p total_array = {l: 0, w: 0, c: 0}#[0, 0, 0]
 	unless params.values.any?
 		files.each do |file|
 			lines = File.read(file)
 			print l = count_l(lines)
-			total_array[0] += l.to_i
+			total_array[:l] += l.to_i
 			print w = count_w(lines)
-			total_array[1] += w.to_i
+			total_array[:w] += w.to_i
 			print c = count_c(lines)
-			total_array[2] += c.to_i
+			total_array[:c] += c.to_i
 			print " #{file}"
 			puts 
 		end
@@ -32,18 +30,30 @@ def main #ファイル引数なし
 	else
 		files.each do |file|
 			lines = File.read(file)
-			print count_l(lines) if params['l']
-			print count_w(lines) if params['w']
-			print count_c(lines) if params['c']
+			print l = count_l(lines) if params['l']
+			total_array[:l] += l.to_i
+			print w = count_w(lines) if params['w']
+			total_array[:w] += w.to_i
+			print c = count_c(lines) if params['c']
+			total_array[:c] += c.to_i
 			print " #{file}"
 			puts
 		end
+		total_print_option(total_array, params) if files.count >= 2
 	end
+end
+
+def total_print_option(total_array, params)
+	total_array.zip(params) do |each_total, opt|
+		print each_total[1].to_s.rjust(SIZE_INDENT) if opt[1] == true
+	end
+	print " total"
+	puts
 end
 
 def total_print(total_array)
 	total_array.each do |each_total|
-		print each_total.to_s.rjust(SIZE_INDENT)
+		print each_total[1].to_s.rjust(SIZE_INDENT)
 	end
 	print " total"
 	puts
